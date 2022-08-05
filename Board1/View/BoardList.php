8fe0edcc8f12fa772ebe9e9c2bf5fd5ea9c23e49
DNWissac <issac1162@danawa.com>
2022-08-05 15:52:22 +0900

    220805 phptest

---
+++ Board1/View/BoardList.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
+<script>
+
+	$().ready(function()
+	{
+	});
+	
+	
+</script>
+
+<title>메인페이지</title>
+
+<!-- 부트스트랩 css, js-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
+rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<button type="button" class="btn btn-primary">유저게시판</button>
+</body>
+</html>
